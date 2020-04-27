6a2e81d4ecce82d6cb2d3668315eb436693ec16e
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-04-27 16:43:58 +0000

    Se acaba el ejercicio

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var pronoun = ['the', 'our'];
 var adj = ['great', 'big'];
 var noun = ['jogger', 'racoon'];
 
+var domainGenerator = "";
+var countIndex = 0;
+
 for ( let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-            document.write(pronoun[i] + adj[j] + noun[k] + ".com" + " "); 
+            domainGenerator+="<li>" + pronoun[i] + adj[j] + noun[k] + ".com" + "</li>";
         }
     }
 }
+console.log(domainGenerator);
+document.querySelector("#domainList").innerHTML = domainGenerator;
